bad0c5147f012a8aa9172f54c72f410f31716871
Pistos <gitsomegrace.5.pistos@geoshell.com>
2008-05-11 00:45:45 -0400

    Now printing payload as it is received.

--- server.rb
+++ server.rb
@@ -12,13 +12,14 @@ require 'socket'
 class App
   def self.rude_comment
     res = Rack::Response.new 
-    res.write "Be gone, foul creature of the internet." 
+    res.write "Be gone, foul creature of the Internet." 
     res.finish
   end
   
   def self.call( env )
     req = Rack::Request.new( env )
     payload = req.POST["payload"]
+    puts payload
     
     return rude_comment if req.get? or payload.nil?
     
@@ -28,7 +29,7 @@ class App
     socket.close
     
     res = Rack::Response.new 
-    res.write "Thanks! You beautiful soul you."
+    res.write "Thanks! You beautiful soul, you."
     res.finish
   end
 
