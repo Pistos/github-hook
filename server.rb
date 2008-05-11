abd5b036d427fbd4d50c95f2746dc3dbdfcff69f
Pistos <gitsomegrace.5.pistos@geoshell.com>
2008-05-11 00:26:34 -0400

    Functional and working with Reby github-hook.rb.

--- server.rb
+++ server.rb
@@ -1,11 +1,13 @@
 #!/usr/bin/env ruby
 
 # Base server code by raggi
+# http://github.com/raggi/github_post_receive_server/
 
 require 'rubygems'
 require 'rack'
 require 'json'
 require 'pp'
+require 'socket'
 
 class App
   def self.rude_comment
@@ -20,8 +22,10 @@ class App
     
     return rude_comment if req.get? or payload.nil?
     
-    payload = JSON.parse( payload )
-    puts payload.pretty_inspect
+    # Ping the Reby EventMachine listening for events
+    socket = TCPSocket.new( '127.0.0.1', 9005 )
+    socket.puts payload
+    socket.close
     
     res = Rack::Response.new 
     res.write "Thanks! You beautiful soul you."
